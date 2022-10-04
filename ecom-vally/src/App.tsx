8fd7a9891249062da091f3d7f49770c943ba7454
PriontoAbdullah <priontosoft@gmail.com>
2022-10-04 09:17:02 +0600

    Added initial project

---
+++ ecom-vally/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Header from './components/common/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
